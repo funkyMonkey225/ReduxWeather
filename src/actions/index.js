72264f0156184e31d248ccef4141b3c92b340abe
Mac User <sabbey37@gmail.com>
2017-09-10 23:45:23 -0400

    Adds axios node module

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "ef1134a1c540f74662b2dd9c876f9c2d";
+const ROOT_URLS = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// Better to define actions as constants so they are easily edited and passed between actions/reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
