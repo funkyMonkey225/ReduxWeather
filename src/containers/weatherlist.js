37d5c32599e21f175baa34373a3eced4229d4ad3
Mac User <sabbey37@gmail.com>
2017-09-11 13:15:32 -0400

    Adds style to Google map

--- src/containers/weatherlist.js
+++ src/containers/weatherlist.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/googlemap';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
@@ -9,15 +10,16 @@ class WeatherList extends Component {
         const tempsF = temps.map(temp => temp * 9/5 - 459.67);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
         
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={tempsF} color={'purple'} units="F"/></td>
                 <td><Chart data={pressures} color={'blue'} units="hPa"/></td>
                 <td><Chart data={humidities} color={'green'} units="%"/></td>
             </tr>
-        )
+        );
     }
     render() {
         return (
